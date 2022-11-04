28bd8898868ae4c7d26984501b848789e1ac1485
anthoniusadi <anthonyfix0@gmail.com>
2022-11-03 17:57:53 -1000

    add sensor parallax

---
+++ ping.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+# dibawah pin tx rx
+GPIO.setup(18,GPIO.IN)
+def read_depth(pin):
+    const = 34000
+    GPIO.setup(GPIO.OUT)
+    GPIO.output(pin,0)
+    time.sleep(0.000002)
+    GPIO.output(pin,1)
+    time.sleep(0.000005)
+    GPIO.output(pin,0)
+    GPIO.setup(pin,GPIO.IN)
+    
+    while GPIO.input(pin)==0:
+        start = time.time()
+    while GPIO.input(pin)==1:
+        end = time.time()
+    t = end-start
+    jarak = t*(const/2)
+    return jarak
